91c31c4409c9de23e4620cdeef8801a31880ff45
GiuliaSossai <giulia.sossai@yahoo.it>
2021-11-22 14:54:44 +0100

    bonus errore

--- vue.js
+++ vue.js
@@ -3,33 +3,41 @@ const app = new Vue ({
 
    data: {
       errorMsg: false,
-      mail: ''
-
-   },
-
-   mounted(){
-      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-         .then((response) =>{
-            console.log('intero oggetto', response);
-
-            const data = response.data;
-            console.log('oggetto data:', data);
-            console.log('mail ottenuta', data.response);
-
-            this.mail = data.response;
-         })
-
-         .catch((error) => {
-            console.log(error);
-            this.errorMsg = true;
-         })
-
+      mail: '',
+      emails: []
 
    },
 
    methods: {
+      getMail(){
+         for(let i = 0; i < 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+               .then((response) =>{
+                  //console.log('intero oggetto', response);
 
+                  const data = response.data;
+                  //console.log('oggetto data:', data);
+                  console.log('mail ottenuta', data.response);
 
+                  this.mail = data.response;
+
+                  this.emails.push(this.mail);
+                  console.log('array emails', this.emails);
+                  
+               })
+
+               .catch((error) => {
+                  console.log(error);
+                  this.errorMsg = true;
+               })      
+         }
+         
+      }
+
+   },
+
+   mounted(){
+      this.getMail();
    }
 
 })
